6b52b92fbcaa7320806e75fb8aee734be1aac7bb
brandonjyan <yanjbrandon@gmail.com>
2022-05-24 22:27:49 -0700

    Postpage added to frontend
    When user clicks on post, they are redirected to a postpage.

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  profilePicture: { type: Image },
+  profilePicture: { type: String },
   id: { type: String },
 });
 
--- frontend/src/Posts/index.js
+++ frontend/src/Posts/index.js
@@ -4,7 +4,7 @@ import IndivPosts from './IndivPosts';
 import * as api from '../api/index.js';
 import Header from '../shared/Header';
 import Channels from './Channels';
-import uclabanner from '../assets/uclabanner.png'
+import uclabanner from '../assets/uclabanner.png';
 
 function checkIfDateInRange(timeOfPost, currTime, range) {
   const newPostTime = timeOfPost.getTime();
@@ -24,7 +24,6 @@ function Posts() {
   async function getPosts() {
     try {
       const response = await api.fetchPosts();
-      console.log(response);
       let searchRes = [...response.data];
       if (response !== undefined || response.size() !== 0) {
         searchRes = searchRes.reverse();
@@ -36,13 +35,12 @@ function Posts() {
           response.data.forEach((element) => {
             let index = searchRes.indexOf(element);
             if (index !== -1) {
-              if(!channelArray.includes(element.channel)) {
+              if (!channelArray.includes(element.channel)) {
                 searchRes.splice(index, 1);
               }
             }
-          })
-        }
-        else {
+          });
+        } else {
           if (query.has('channel')) {
             const channel = query.get('channel');
             response.data.forEach((element) => {
@@ -80,7 +78,6 @@ function Posts() {
             });
           }
         }
-        
       }
       setPosts(searchRes);
     } catch (error) {
@@ -103,23 +100,17 @@ function Posts() {
           <a href={'/submit'}>
             <button id='submit_button'>Submit a post!</button>
           </a>
-          <Channels/>
+          <Channels />
         </div>
         <div id='indiv-posts'>
-          {
-            (() => {
-              if (posts === undefined)
-                return <h1>No posts.</h1>
-              else if (posts.length === 0)
-                return <h1>No posts.</h1>
-              else 
-                return (
-                  posts.map(function (currVal) {
-                    return <IndivPosts _id={currVal._id} title={currVal.title} message={currVal.message} channel={currVal.channel} creator={currVal.creator} createdAt={currVal.createdAt} />;
-                  })
-                )
-            })()
-          }
+          {(() => {
+            if (posts === undefined) return <h1>No posts.</h1>;
+            else if (posts.length === 0) return <h1>No posts.</h1>;
+            else
+              return posts.map(function (currVal) {
+                return <IndivPosts _id={currVal._id} title={currVal.title} message={currVal.message} channel={currVal.channel} creator={currVal.creator} createdAt={currVal.createdAt} />;
+              });
+          })()}
           {/* {posts === undefined ? (
             <h1>loading</h1>
           ) : (
